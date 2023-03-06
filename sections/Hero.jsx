9ab10324b61f75cc57eec395e85dda6a5e3e75d6
vaibhavb479 <vaibhavb479@gmail.com>
2023-03-06 23:20:17 +0530

    name change

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,15 +16,15 @@ const Hero = () => (
     >
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          AAVIANS
+        {' <aavians/>'}
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
-          <h1 className={styles.heroHeading}>T</h1>
-          {/* <div className={styles.heroDText} /> */}
-          <h1 className={styles.heroHeading}>ECH</h1>
+          {/* <h1 className={styles.heroHeading}>T</h1>
+          <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>ECH</h1> */}
         </motion.div>
       </div>
 
