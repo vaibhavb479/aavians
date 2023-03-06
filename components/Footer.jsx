d8c1f9c5504f082dff815c1d897b6b7ac19a633c
vaibhavb479 <vaibhavb479@gmail.com>
2023-03-06 23:00:15 +0530

    double quote

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => (
 
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">
-            {" <aavians/>"}
+            {' <aavians/'}
           </h4>
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2023 AAVIANS. All rights reserved.
