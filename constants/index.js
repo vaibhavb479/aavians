69049a474c3e974fed32af3f24e8ca54dc3d0bc1
vaibhavb479 <vaibhavb479@gmail.com>
2023-03-06 22:34:58 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,85 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/planet-01.png",
+    title: "Indian Food Truck",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/planet-02.png",
+    title: "Way 2 Promotions",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/planet-03.png",
+    title: "Tanvi Healthcare",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/planet-04.png",
+    title: "3D Portfolio",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/planet-05.png",
+    title: "Songs24",
+  },
+];
+
+export const startingFeatures = [
+  "Schedule a consultation with us.",
+  "During the consultation, discuss your needs and provide a detailed explanation of your requirements.",
+  "Provide feedback and ensure that the project is progressing according to the agreed timeline and milestones.",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "Consultation",
+    subtitle:
+      "we have the best industry people who can help you in the whole startup process",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "Technology",
+    subtitle:
+      "we have professionally experienced people who use the latest technology that can boost your online presence ",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/planet-06.png",
+    title: "Why Us?",
+    subtitle: "Why Businesses and Startup needs online presence in 2023",
+  },
+  {
+    imgUrl: "/planet-07.png",
+    title: "Why Websites?",
+    subtitle: "Why Website is needed for any business or startup",
+  },
+  {
+    imgUrl: "/planet-08.png",
+    title: "Why Digital Marketing",
+    subtitle: "Why Digital Marketing is needed for any business or startup",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About AAVIANS" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">AAVIANS </span>are excited
+        to launch our IT service project! This innovative new service will
+        provide affordable, reliable IT support to businesses in the area. Our
+        team of
+        <span className="font-extrabold text-white">
+          {" "}
+          experienced professionals
+        </span>{" "}
+        will be available 24/7 to help with any IT needs you may have. We look
+        forward to helping your business run smoothly and efficiently!
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
